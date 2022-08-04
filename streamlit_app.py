d3c94370ee4731e8ca98378139de5caa054b2357
hitsmaverick11 <42539937+hitsmaverick11@users.noreply.github.com>
2022-08-04 10:40:23 +0530

    Update streamlit_app.py
    added some custom images for menu and a header file is added

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,8 @@ streamlit.title('First streamlit Program')
 
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, spinach & rocket smoothie')
-streamlit.text('Hard-boiled Free-Range egg')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, spinach & rocket smoothie')
+streamlit.text('🐔Hard-boiled Free-Range egg')
+streamlit.text('🥑🍞 Avacado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
